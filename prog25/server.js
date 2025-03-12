72dd07597fa99ee1f5b3c1713b87572140c33704
AxelBossMan <axwoxen@gmail.com>
2025-03-12 15:24:47 +0100

    Add files via upload

---
+++ prog25/server.js
@@ -0,0 +1,32 @@
+const express = require(`express`)
+const app = express()
+
+app.use(express.json());
+app.use(express.static('public'));
+
+app.set('view engine', 'ejs');
+
+const port = 3000;
+
+app.get('/', function (req, res) {
+    res.render('frontend');  
+});
+
+app.get('/logInn', function (req, res) {
+    res.render('logInn');
+});
+app.get('/bliKunde', function (req, res) {
+    res.render('bliKunde');
+});
+
+
+app.get('/hjemmeside', function (req, res) {
+    res.render('hjemmeside');
+});
+
+
+
+app.listen(port, () => {
+    console.log(`Server running on http://localhost:${port}`);
+});
+
