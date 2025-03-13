11a8822490551d0c815d1de21b474721eb57cb9b
LinusPalm1 <linuspq@gmail.com>
2025-03-13 08:48:47 +0100

    Dåse

--- prog25/server.js
+++ prog25/server.js
@@ -15,7 +15,7 @@ app.get('/', function (req, res) {
 //test 1 
 //tiss tass
 
-
+//linus
 //Anders
 
 
@@ -36,4 +36,3 @@ app.get('/hjemmeside', function (req, res) {
 app.listen(port, () => {
     console.log(`Server running on http://localhost:${port}`);
 });
-
