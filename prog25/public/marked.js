b7c4133eebef4d8b45953b1ba00004ed113b7b8b
MarcusGrodem <marcus.grude.grodem@gmail.com>
2025-03-14 10:16:32 +0100

    la til børs og marked

--- prog25/public/marked.js
+++ prog25/public/marked.js
@@ -1,8 +1,38 @@
-async function fetchStockData() {
-    const response = await fetch("https://www.alphavantage.co/query?function=TIME_SERIES_DAILY&symbol=IBM&apikey=4B5HBYMRCDCC4A6M");
-    const data = await response.json();
-    console.log(data);
-}
-fetchStockData();
+
+
+document.addEventListener('DOMContentLoaded', async() =>{
+    try{
+        const response = await fetch("https://www.alphavantage.co/query?function=TIME_SERIES_DAILY&symbol=IBM&apikey=4B5HBYMRCDCC4A6M");
+        const data = await response.json();
+        console.log(data);
+
+        const ctx = document.getElementById(`chart`).getContext(`2d`);
+        new Chart(ctx, {
+            type: "line",
+            data: {
+                labels: data.labels,
+                datasets: [{
+                    label: "Meta",
+                    data: data.close,
+                    bordercolor: "blue",
+                    backgroundColor: "lightblue",
+                    borderWidth: 2,
+                    pointRadius: 3
+                }]
+            },
+            options:{
+                responsive: true,
+                scales: {
+                    x: {title: {display: true, text: "Date"}},
+                    y: {title: {display: true, text: "Price"}}
+                }
+            }   
+        });
+    } catch(error){
+        console.error("Error: ", error);
+    }
+});
+
+
 
 
